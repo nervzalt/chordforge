6a918ce0bb6f47b908890d9742c6aec53c73df72
nervzalt <nervzofficial@gmail.com>
2026-03-25 12:06:35 +0100

    Create src/main.jsx file

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChordForge from './ChordForge'
+ReactDOM.createRoot(document.getElementById('root')).render(<ChordForge />)
